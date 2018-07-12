736fdfbdcc7261db67f532cf7b49e504b217ecfc
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-12 00:34:43 -0400

    -edit to root route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const server     = express();
 
 server.use(express.static(path.join(__dirname, 'build')));
 
-server.get('/*', (req, res) => {
+server.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
