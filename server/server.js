416ac7f5a3113604c6696bf646754d33b08116cf
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-12 10:02:39 -0400

    -yarn added morgan for serverside and axios for client side

--- server/server.js
+++ server/server.js
@@ -4,21 +4,31 @@ const bodyParser = require('body-parser');
 const path       = require('path');
 const cors       = require('cors');
 const helmet     = require('helmet');
+const morgan     = require('morgan');
 
 require('dotenv').config()
 
 const server     = express();
 
-server.use(express.static(path.join(__dirname, 'build')));
-server.use(cors({origin: "localhost://3000"}));
+server.use(express.static(path.join(__dirname, 'client/build')));
+server.use(cors());
 server.use(helmet());
+server.use(morgan('combined'))
+server.use(allowCrossDomain);
+
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  }
 
 server.get('/', (req, res ) => {
     res.status(200).json({api: 'app and running'})
 })
 
 server.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+   res.sendFile(path.join(__dirname, 'client/build'))
 });
 
 
