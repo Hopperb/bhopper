711610c4517381ab2ecaaeb1f2c34b81486ef7e7
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-12 00:54:47 -0400

    -yarn added cors and helmet -user cors origin and helmet method -added root route for api again for sanity

--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ const express    = require('express');
 const mongoose   = require('./db/mongoose');
 const bodyParser = require('body-parser');
 const path       = require('path');
+const cors       = require('cors');
+const helmet     = require('helmet');
 
 const server     = express();
 
 server.use(express.static(path.join(__dirname, 'build')));
+server.use(cors({origin: "localhost://3000"}));
+server.use(helmet());
 
-server.get('/', (req, res) => {
+server.get('/', (req, res ) => {
+    res.status(200).json({api: 'app and running'})
+})
+
+server.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
